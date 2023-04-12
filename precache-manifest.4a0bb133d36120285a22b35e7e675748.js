af1059427c58e6fb0ded0cf90d03e39489bca912
Ryan Huang <ruoyu.huang@instabase.com>
2023-04-12 01:00:37 -0700

    Updates

---
+++ precache-manifest.4a0bb133d36120285a22b35e7e675748.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "900ed5128588ba427c55",
+    "url": "/static/css/main.8f03793f.chunk.css"
+  },
+  {
+    "revision": "900ed5128588ba427c55",
+    "url": "/static/js/main.900ed512.chunk.js"
+  },
+  {
+    "revision": "3a79397ac1a1b3a66e79",
+    "url": "/static/js/1.3a79397a.chunk.js"
+  },
+  {
+    "revision": "9d946f9883d3faa165dd",
+    "url": "/static/js/2.9d946f98.chunk.js"
+  },
+  {
+    "revision": "21afeaa482eafbbf4857",
+    "url": "/static/js/runtime~main.21afeaa4.js"
+  },
+  {
+    "revision": "0a25f8885200b8c4d591ed34f74f3852",
+    "url": "/index.html"
+  }
+];
